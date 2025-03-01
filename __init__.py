15952bd180525288228cd7b3802d7a45de7d5b77
Leonardo Brondani Schenkel <leonardo@schenkel.net>
2025-03-01 23:43:22 +0100

    Upgrade version, start using date-based scheme

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@ from calibre.customize import StoreBase
 
 class BokonStore(StoreBase):
     name            = 'Bokon.se'
-    version         = (0,1,0)
+    version         = (2025, 3, 0)
     description     = 'Köp e-böcker i Sveriges e-bokhandel'
     author          = 'Leonardo Brondani Schenkel <leonardo@schenkel.net>'
     actual_plugin   = 'calibre_plugins.lbschenkel_store_bokon_se.bokon:BokonStorePlugin'
